11a5f07011cf6a2ead36166f1ac236924c621fba
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-10-26 18:49:55 +1000

    build: remove vercel integration and tidy up package.json scripts

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+import { remarkReadingTime } from './src/utils/remarkReadingTime'
 import remarkUnwrapImages from 'remark-unwrap-images'
 import rehypeExternalLinks from 'rehype-external-links'
 import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
-
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://example.me',
 	integrations: [
@@ -41,8 +38,5 @@ export default defineConfig({
 		}
 	},
 	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	})
+	output: 'static'
 })
