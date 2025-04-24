435fcafa536c6e7df31d54e95bd0edb906cbf490
Michael Poulgrain <michael.poulgrain@gmail.com>
2025-04-24 20:26:42 +1000

    chore: update dependencies (patch )

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -18,6 +18,7 @@ export default function Globe() {
                 client:only="react"
                 width={400}
                 height={400}
+                enablePointerInteraction={false}
                 showAtmosphere={false}
                 globeImageUrl="https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg"
                 backgroundColor="hsla(0deg, 0%, 0%, 0.0)"
